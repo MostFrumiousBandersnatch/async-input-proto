7d64529022e4d2ff24cbdeb8a30de799c12089ec
Ivan Kondratyev <ivanbright@gmail.com>
2023-02-19 19:16:22 +0300

    Slightly improved types-hingting in tokenizer.

--- packages/widget/src/engine/tokenizer.ts
+++ packages/widget/src/engine/tokenizer.ts
@@ -1,36 +1,38 @@
-import { Token, Tokenizer } from '@widget/engine/types';
-
+import { Token, Tokenizer, TokenWithSuggestions } from '@widget/engine/types';
 
 export const breakString = (raw: string): string[] =>
   raw.split(/\s+/).filter(token => token.length > 0);
 
-export const genTokenId = (token: Token): string => `${token.content}_${token.start}`;
+export const genTokenId = (token: Token): string =>
+  `${token.content}_${token.start}`;
 
-export const tokenProcessor: Tokenizer = raw=> {
-  const tokens = breakString(raw).reduce((parsed, rawPart) => {
-    const prev = parsed.at(-1);
-    const start = raw.indexOf(rawPart, prev?.end || 0);
+export const tokenProcessor: Tokenizer = raw => {
+  const tokens = breakString(raw).reduce<TokenWithSuggestions[]>(
+    (parsed, rawPart) => {
+      const prev = parsed.at(-1);
+      const start = raw.indexOf(rawPart, prev?.end || 0);
 
-    const token: Token = {
-      content: rawPart,
-      start: start,
-      end: start + rawPart.length,
-      spaceBefore: prev ? start - prev.end : start,
-      ghost: false
-    };
+      const token: Token = {
+        content: rawPart,
+        start: start,
+        end: start + rawPart.length,
+        spaceBefore: prev ? start - prev.end : start,
+        ghost: false,
+      };
 
-    return [
-      ...parsed,
-      {
-        ...token,
-       id: genTokenId(token)
-      },
-    ];
-  }, []);
+      return [
+        ...parsed,
+        {
+          ...token,
+          id: genTokenId(token),
+        },
+      ];
+    },
+    []
+  );
 
   return {
     raw,
     parsed: tokens,
   };
 };
-
